656457b00abb00e9fbaf390acaa13d2a7f048d8f
LeviTheories7 <149465748+LeviTheories7@users.noreply.github.com>
2024-02-08 20:42:10 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+window.alert(`Hi! Good Evening, Aliazah! `);
+window.alert(`I Hope You Are Doing Well`);
+window.alert(`There Is Something Wrong about me?`);
+window.alert(`I Thought You understand my Schedule?`);
+window.alert(`I'm not interested na ba?`);
+window.alert(`Sorry if I'm really busy to my Schedule`);
+window.alert(`I hope You will Understand It`);
+window.alert(`And I hope Your will Appriciate This my simple Effort`);
+window.alert(`Goood Night`);
